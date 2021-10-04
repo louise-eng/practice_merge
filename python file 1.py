0cba8bddf0ae9228ddfba3db14c93cb165d6c253
Louise Romaniuk <elleromaniuk@hotmail.co.uk>
2021-10-04 18:07:37 +0100

    text

---
+++ python file 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 18:05:19 2021
+
+@author: eller
+"""
+
+erwgvbfudygsufyd
+dsfcyhdsb'
+dsfdbsFVD
+DFSFCDS
+DSFDS
+
+DSFDSfg
+hgngfmnhgf
+cdsdgbfdbh
+fdghfdhfgdh
+sdsgegeh
